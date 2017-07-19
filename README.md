c6ca8dd7f3799948bfea43ee650c33d8fdceb38c
Kyle Lim <kyle.lim@ngc.com>
2017-07-19 10:35:43 -0400

    Added trainNN.py Program
    The new program trains a neural network excessively to solve a small
    problem. Also added in the README.md documentation.

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Make it look like you're working
 <b>hexSearch.sh</b> - Randomly outputs hexidecimal numbers and highlights
 random numbers to make it simulate a search.
 
+
 <b>fakeCompilation.sh</b> - Emulates gcc by pulling lines from /usr/include and
 including them with a gcc flag.
+
 
 <b>fakeBitbake.sh</b> - Simulates a bitbake build. It requires the three files:
 fakeBitbake.sh, bitbake.sh, and bitbakeargs.txt. It works by outputting the
@@ -13,7 +15,16 @@ normal bitbake text and some fake warnings and looping through an array of
 bitbake arguments. It then uses echo carriages to change the tasks that it is
 "bitbaking" to simulate that is it building something.
 
-Note: Must use "pkill bash" to end due to background processes
+Note: Must use "pkill bash" to end due to background processes. This may crash
+other programs that are running bash. Use with caution!
+
 
 <b>everything.sh</b> - Simply outputs everything from every file on your system.
-Use with Caution!
+Use with caution!
+
+
+<b>trainNN.py</b> - Creates a neural network and trains it to solve the XOR
+problem. The catch is that it outputs its training process every cycle for
+a million cycles.
+
+Note: Must have numpy python module installed.
---
+++ trainNN.py
@@ -0,0 +1,68 @@
+from numpy import exp, array, random, dot
+
+class NeuronLayer():
+    def __init__(self, number_of_neurons, number_of_inputs_per_neuron):
+        self.synaptic_weights = 2 * random.random((number_of_inputs_per_neuron, number_of_neurons)) - 1
+
+class NeuralNetwork():
+    def __init__(self, layer1, layer2):
+        self.layer1 = layer1
+        self.layer2 = layer2
+
+    # Sigmoid to normalize between 0 and 1
+    def __sigmoid(self, x):
+        return 1 / (1 + exp(-x))
+    # Sigmoid derivative
+    def __sigmoid_derivative(self, x):
+        return x * (1 - x)
+
+    # Training
+    def train(self, training_set_inputs, training_set_outputs, number_of_training_iterations):
+        for i in xrange(number_of_training_iterations):
+            # Pass training set
+            output_from_layer_1, output_from_layer_2 = self.think(training_set_inputs)
+            # Calculate error
+            layer2_error = training_set_outputs - output_from_layer_2
+            layer2_delta = layer2_error * self.__sigmoid_derivative(output_from_layer_2)
+            # Calculate error
+            layer1_error = layer2_delta.dot(self.layer2.synaptic_weights.T)
+            layer1_delta = layer1_error * self.__sigmoid_derivative(output_from_layer_1)
+            # Calculate adjustment
+            layer1_adjustment = training_set_inputs.T.dot(layer1_delta)
+            layer2_adjustment = output_from_layer_1.T.dot(layer2_delta)
+            # Adjust the weights.
+            self.layer1.synaptic_weights += layer1_adjustment
+            self.layer2.synaptic_weights += layer2_adjustment
+
+
+    def think(self, inputs):
+        output_from_layer1 = self.__sigmoid(dot(inputs, self.layer1.synaptic_weights))
+        output_from_layer2 = self.__sigmoid(dot(output_from_layer1, self.layer2.synaptic_weights))
+        # Useless output to print
+        print output_from_layer1
+        return output_from_layer1, output_from_layer2
+
+if __name__ == "__main__":
+
+    #Seed the random number generator
+    random.seed(1)
+    # Create layer 1 (4 neurons, each with 3 inputs)
+    layer1 = NeuronLayer(4, 3)
+
+    # Create layer 2 (a single neuron with 4 inputs)
+    layer2 = NeuronLayer(1, 4)
+
+    # Combine the layers to create a neural network
+    neural_network = NeuralNetwork(layer1, layer2)
+
+    # XOR Training set
+    training_set_inputs = array([[0, 0, 1], [0, 1, 1], [1, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1], [0, 0, 0]])
+    training_set_outputs = array([[0, 1, 1, 1, 1, 0, 0]]).T
+    # Train
+    print "Training Neural Network"
+    neural_network.train(training_set_inputs, training_set_outputs, 1000000)
+
+    # Test the neural network with a new situation.
+    print "Considering a new situation [1, 1, 0] -> ?: "
+    hidden_state, output = neural_network.think(array([1, 1, 0]))
+    print output
